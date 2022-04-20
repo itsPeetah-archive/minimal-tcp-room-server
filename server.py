3c8e883e42c2e2920d12230a9cb6974da53999fe
itsPeetah <pietro.g.moroni@gmail.com>
2022-04-20 20:34:03 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from socket import *
+from atexit import register
+from socket import socket, AF_INET, SOCK_STREAM
 from threading import Thread
+from time import sleep
+
+from numpy import broadcast
 
 # Settings ======================================================
 # Network
 defaultHostname = "localhost"
 defaultPort = 42069
+# Misc
+UTF8 = "utf-8"
 
 
 class TCPServer:
@@ -23,30 +29,56 @@ class TCPServer:
 
         while True:
             newSocket, clientAddress = TCPServer.socket.accept()
-            Thread(target=ServerMethods.newConnectionHandler,
+            Thread(target=TCPServer.newConnectionHandler,
                    args=[newSocket, clientAddress]).start()
 
     @staticmethod
+    def newConnectionHandler(sock: socket, addr):
+        room = sock.recv(2048).decode(UTF8)
+        id = TCPServer.addClientToRoom(sock, addr, room)
+
+        # Build send message function
+        def sendMessage(msg): return TCPServer.broadcast(
+            sock, addr, room, id, msg)
+
+        def clientListener():
+            TCPServer.registerClientForBroadcasting(sock, addr, room)
+            try:
+                while True:
+                    msg = sock.recv(2048).decode(UTF8)
+                    Thread(target=sendMessage, args=(msg)).start()
+            except:
+                TCPServer.removeClient(sock, addr)
+
+        sock.send(str(id).encode(UTF8))
+        sleep(1)
+        Thread(target=clientListener).start()
+
+    @staticmethod
     def addClientToRoom(sock, addr, room):
+        if room not in TCPServer.rooms:
+            TCPServer.rooms[room] = [(sock, addr)]
+            return 0
+        else:
+            TCPServer.rooms[room].append((sock, addr))
+            # Not sure about thread safety, I'm going to look for the right element
+            for i, t in enumerate(TCPServer.rooms[room]):
+                if t[0] == sock:
+                    return i
+            # Should not be needed
+            return i
+
+    @staticmethod
+    def registerClientForBroadcasting(sock, addr, room):
+        # TCPServer.clients[sock]
         pass
 
     @staticmethod
     def removeClient(sock, addr):
         pass
 
-
-class ServerMethods:
-
     @staticmethod
-    def newConnectionHandler(sock, addr):
-        pass
-
-    @staticmethod
-    def clientListener(sock, addr):
-        pass
-
-    @staticmethod
-    def clientSpeaker(sock, addr):
+    def broadcast(sock, addr, room, id, msg):
         pass
 
 
